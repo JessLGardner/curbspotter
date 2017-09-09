0df468efb850e63b5a0b95bca8a40840fb6392ef
Jess Gardner <inanna1130@gmail.com>
2017-09-08 21:57:29 -0400

    Created create, update, destroy routes in Post controller.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -14,6 +14,44 @@ class Api::PostsController < ApplicationController
     render json: @post 
   end
 
+  def create
+    @neighborhood = Neighborhood.find params[:neighborhood_id]
+    @post = @neighborhood.posts.new(post_params)
+
+    if @post.save
+      render json: @post
+    else
+      render json: {
+        message: 'error creating post'
+      }
+    end
+  end
+
+  def update
+    @neighborhood = Neighborhood.find params[:neighborhood_id] 
+    @post = @neighborhood.posts.find params[:id]
+    @post.update(post_params)
+    
+    if @post.save
+      render json: @post
+    else
+      render json: {
+        message: 'error updating post'
+      }
+    end
+  end
+
+  def destroy
+    @neighborhood = Neighborhood.find params[:neighborhood_id]
+    @post = @neighborhood.posts.find params[:id]
+    @post.destroy
+
+    render json: {
+      message: 'creature successfully destroyed'
+    }
+  end
+
+
   private
   def post_params
     params.require(:post).permit(:title, :content, :image_url, :category, :location, :neighborhood_id, :created_at)
